79c23463f1fb29dd74d185e4ba3f63ad1dd736df
Konlll <hungarokornel@gmail.com>
2019-01-20 20:36:05 +0100

    A jQuery elevateZoom plugin beillesztése:   - a nike_man_shoes és a nike_woman_shoes-nál a képekre aktiváltuk ezt az elevateZoom függvényt a main.js-ben, ezáltal ha rámegyünk a képre, akkor ránagyít és ehhez kellett a képeknek egy nagyobbított másolatot csinálni, hogy működjön az egész
    1-2 fix: a nike_man_shoes.html-ben linkeljen a nike_woman_shoes.html-re.

--- js/main.js
+++ js/main.js
@@ -11,4 +11,65 @@ $(document).ready(function() {
     },
     function() {}
   );
+
+  // Shoes
+  $("#zoom_01").elevateZoom({
+    /*zoomType: "lens",
+    lensShape: "round",
+    lensSize: 170*/
+
+    easing: true,
+    zoomType: "inner",
+    cursor: "crosshair"
+  });
+
+  $("#zoom_02").elevateZoom({
+    /*zoomType: "lens",
+    lensShape: "round",
+    lensSize: 170*/
+
+    easing: true,
+    zoomType: "inner",
+    cursor: "crosshair"
+  });
+
+  $("#zoom_03").elevateZoom({
+    /*zoomType: "lens",
+    lensShape: "round",
+    lensSize: 170*/
+
+    easing: true,
+    zoomType: "inner",
+    cursor: "crosshair"
+  });
+
+  $("#zoom_04").elevateZoom({
+    /*zoomType: "lens",
+    lensShape: "round",
+    lensSize: 170*/
+
+    easing: true,
+    zoomType: "inner",
+    cursor: "crosshair"
+  });
+
+  $("#zoom_05").elevateZoom({
+    /*zoomType: "lens",
+    lensShape: "round",
+    lensSize: 170*/
+
+    easing: true,
+    zoomType: "inner",
+    cursor: "crosshair"
+  });
+
+  $("#zoom_06").elevateZoom({
+    /*zoomType: "lens",
+    lensShape: "round",
+    lensSize: 170*/
+
+    easing: true,
+    zoomType: "inner",
+    cursor: "crosshair"
+  });
 });
